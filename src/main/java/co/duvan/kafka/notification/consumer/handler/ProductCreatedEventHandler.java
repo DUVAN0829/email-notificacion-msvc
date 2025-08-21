ac43de1c7df39124fa12fe547585d10344c2ea95
DUVAN0829 <duvancasgonz29@gmail.com>
2025-08-21 18:01:16 -0500

    Añade dependencia 'co.duvan.kafka.core.coremsvc.ProductCreatedEvenet'

--- src/main/java/co/duvan/kafka/notification/consumer/handler/ProductCreatedEventHandler.java
+++ src/main/java/co/duvan/kafka/notification/consumer/handler/ProductCreatedEventHandler.java
@@ -1,10 +1,21 @@
 package co.duvan.kafka.notification.consumer.handler;
 
+import co.duvan.kafka.core.coremsvc.ProductCreatedEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProductCreatedEventHandler {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
+    @KafkaListener(topics = "product-created-events-topic")
+    public void handler(ProductCreatedEvent productCreatedEvent) {
+
+        LOGGER.info("Received a new event {}", productCreatedEvent.getTittle());
+
+    }
 
 }
